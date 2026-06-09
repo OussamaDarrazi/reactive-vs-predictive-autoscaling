e9c778de6ba3e2b234247bb7fe7ce414bc3e93e6
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2026-06-09 14:11:23 +0200

    remove disable traefik in bootstrap script

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -151,7 +151,7 @@ install_k3s() {
   fi
 
   log_info "Installing k3s..."
-  curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" sh -
+  curl -sfL https://get.k3s.io | sh -
   log_success "K3s installed"
 }
 
